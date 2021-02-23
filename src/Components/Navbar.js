3b612b816b9a911cd0419596feda5e92ef4dc455
kungpaockn <kevingoenadibrata@gmail.com>
2021-02-23 00:42:05 -0800

    mobile ready

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,25 +1,28 @@
 import React from "react";
 import Logo from "../Assets/logo.png";
 import { animateScroll as scroll } from "react-scroll";
+import { BrowserView } from "react-device-detect";
+
 const Navbar = () => {
   return (
     <div className="NavbarContainer">
       <div className="Navbar">
-        <img className="logo" src={Logo} />
-
-        <div className="navi">
-          <a href="https://app.hirewithpiper.com">
-            <h4>Log In</h4>
-          </a>
-          <button
-            onClick={() => {
-              scroll.scrollToBottom();
-            }}
-            className="button-sm"
-          >
-            Get Started
-          </button>
-        </div>
+        <img className="logo" src={Logo} alt="logo" />
+        <BrowserView>
+          <div className="navi">
+            <a href="https://app.hirewithpiper.com">
+              <h4>Log In</h4>
+            </a>
+            <button
+              onClick={() => {
+                scroll.scrollToBottom();
+              }}
+              className="button-sm"
+            >
+              Get Started
+            </button>
+          </div>
+        </BrowserView>
       </div>
     </div>
   );
